8fc9d85dff6420d724a2f7b985e79225e5562e7a
Christian Mwamba <chrismwamba9@gmail.com>
2022-11-26 03:40:04 +0200

    adding search bar and logo

--- lib/app/widgets/views/header.dart
+++ lib/app/widgets/views/header.dart
@@ -26,19 +26,79 @@ AppBar Header() {
                     color: AppColorTheme.textColor,
                   ))
               : Container(),
-          Text(
-            'I N T E R N E W S 🇨🇩',
-            style: GoogleFonts.roboto(
-                textStyle: const TextStyle(
-                    color: AppColorTheme.primaryColor,
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold)),
+          Container(
+            height: 70,
+            width: 210,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/logo.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
           ),
+          (controller.width > 600)
+              ? const SizedBox(
+                  width: 120,
+                )
+              : Container(),
+          (controller.width > 750)
+              ? SizedBox(
+                  height: 46,
+                  width: 495,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Rechercher une information',
+                      hintStyle: GoogleFonts.roboto(
+                        color: AppColorTheme.textColor.withOpacity(0.3),
+                        fontSize: 16,
+                        fontWeight: FontWeight.w400,
+                      ),
+                      suffixIcon: const Icon(
+                        Icons.search,
+                        color: AppColorTheme.textColor,
+                      ),
+                      filled: true,
+                      fillColor: AppColorTheme.primaryColor.withOpacity(0.04),
+                      //hoverColor: AppColorTheme.textColor,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(4),
+                        borderSide: const BorderSide(
+                          color: AppColorTheme.textColor,
+                        ),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(4),
+                        borderSide: BorderSide(
+                          width: 0,
+                          color: AppColorTheme.textColor.withOpacity(0),
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(4),
+                        borderSide: BorderSide(
+                          color: AppColorTheme.primaryColor,
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              : Container(),
         ],
       ),
     ),
     automaticallyImplyLeading: false,
     actions: [
+      (controller.width < 600)
+          ? IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.search,
+                color: AppColorTheme.textColor,
+              ))
+          : Container(),
+      SizedBox(
+        width: 12,
+      ),
       SizedBox(
         height: 40,
         child: Padding(
