91015f5dd5c8d89675ceda1f6e84cf9a9da6422a
Christian Mwamba <chrismwamba9@gmail.com>
2022-12-06 00:02:37 +0200

    education contents controller

--- lib/app/modules/home/views/screens/education/controllers/education.controller.dart
+++ lib/app/modules/home/views/screens/education/controllers/education.controller.dart
@@ -1,12 +1,21 @@
 import 'package:get/get.dart';
+import 'package:mediaapp/app/data/models/news_model.dart';
+import 'package:mediaapp/app/data/repository/news_repository.dart';
 
 class EducationController extends GetxController {
-  //TODO: Implement EducationController
+  final NewsRepository _newsRepository = new NewsRepository();
 
-  final count = 0.obs;
+  final _newsModel = NewsModel().obs;
+  get newsModel => _newsModel.value;
+  set newsModel(value) => _newsModel.value = value;
+
+  final _newsList = <NewsData>[].obs;
+  List<NewsData> get newsList => _newsList;
+
   @override
   void onInit() {
     super.onInit();
+    getAll();
   }
 
   @override
@@ -19,5 +28,10 @@ class EducationController extends GetxController {
     super.onClose();
   }
 
-  void increment() => count.value++;
+  getAll() {
+    _newsRepository.getAll().then((NewsModel? data) {
+      newsModel = data;
+      _newsList.value = newsModel.data!;
+    });
+  }
 }
