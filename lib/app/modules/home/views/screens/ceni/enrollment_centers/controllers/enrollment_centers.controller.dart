5f026f4aa47ed6d494952d29f90daea6d853df74
Christian Mwamba <chrismwamba9@gmail.com>
2022-12-06 00:04:56 +0200

    enrollment center controller

--- lib/app/modules/home/views/screens/ceni/enrollment_centers/controllers/enrollment_centers.controller.dart
+++ lib/app/modules/home/views/screens/ceni/enrollment_centers/controllers/enrollment_centers.controller.dart
@@ -1,12 +1,24 @@
 import 'package:get/get.dart';
+import 'package:mediaapp/app/data/models/centres_model.dart';
+import 'package:mediaapp/app/data/repository/centers_repository.dart';
 
 class EnrollmentCentersController extends GetxController {
-  //TODO: Implement EnrollmentCentersController
+  final CentersRepository _andidatsRepository = new CentersRepository();
 
-  final count = 0.obs;
+  final _centresModel = CentresModel().obs;
+  get centresModel => _centresModel.value;
+
+  final _centresData = <CentresData>[].obs;
+  List<CentresData> get centresData => _centresData;
+
+  final _subpage = ''.obs;
+  String get subpage => _subpage.value;
+
   @override
   void onInit() {
     super.onInit();
+    getAll();
+    _subpage.value = Get.parameters['subpage']!;
   }
 
   @override
@@ -19,5 +31,12 @@ class EnrollmentCentersController extends GetxController {
     super.onClose();
   }
 
-  void increment() => count.value++;
+  getAll() {
+    _andidatsRepository.getAll().then((CentresModel? data) {
+      if (data != null) {
+        _centresModel.value = data;
+        _centresData.value = data.data!;
+      }
+    });
+  }
 }
