5632822cfe292f7baf092e54b16dbf116c92d7fa
Christian Mwamba <chrismwamba9@gmail.com>
2023-03-22 12:23:10 +0200

    edit electoral view

--- lib/app/modules/home/views/screens/ceni/calendar/mobile_calendar.screen.dart
+++ lib/app/modules/home/views/screens/ceni/calendar/mobile_calendar.screen.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:mediaapp/app/core/themes/color_theme.dart';
-import 'package:mediaapp/app/widgets/calandars/mobile_calendar_card.dart';
 import 'package:mediaapp/app/widgets/news_card_shimmer.dart';
 
+import '../../../../../../widgets/calandars/calandar_card.dart';
 import 'controllers/calendar.controller.dart';
 
 class MobileCalendarScreen extends GetView<CalendarController> {
@@ -14,58 +15,90 @@ class MobileCalendarScreen extends GetView<CalendarController> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 8),
-      child: ListView(children: [
-        Text(
-          "Calendrier électoral de la CENI",
-          style: GoogleFonts.roboto(
-              textStyle: const TextStyle(
-                  color: AppColorTheme.textColor,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold)),
-        ),
-        const SizedBox(
-          height: 24,
-        ),
-        Container(
-          height: 250,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(4),
-            boxShadow: [
-              BoxShadow(
-                color: AppColorTheme.darkColor.withOpacity(0.04),
-                blurRadius: 20,
-                offset: const Offset(0, 2),
-              ),
-            ],
-            color: AppColorTheme.whiteColor,
-          ),
-          child: Image.asset(
-            'assets/images/calendar.png',
-            fit: BoxFit.contain,
-          ),
-        ),
-        SizedBox(
-          height: 24,
-        ),
-        Obx(() {
-          return (controller.calandarsList.length > 0)
-              ? ListView.builder(
-                  itemCount: controller.calandarsList.length,
-                  // scrollDirection: Axis.vertical,
-                  primary: false,
-                  shrinkWrap: true,
-                  itemBuilder: (context, index) {
-                    return MobileCalandrCardView(
-                        controller.calandarsList[index], controller.subpage);
-                  })
-              : NewsCardShimmerWidget();
-        }),
-        SizedBox(
-          height: 16,
-        ),
-      ]),
+    return Obx(
+      () => (!controller.isBusy)
+          ? controller.calandarsList.isEmpty
+              ? Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListView(
+                    children: [
+                      Center(
+                        child: Column(
+                          children: [
+                            const SizedBox(
+                              height: 24,
+                            ),
+                            SvgPicture.asset(
+                              'assets/images/no_data.svg',
+                              width: 200,
+                              height: 200,
+                            ),
+                            const SizedBox(
+                              height: 16,
+                            ),
+                            Text('Aucune donnée disponible',
+                                style: TextStyle(
+                                    color: AppColorTheme.textColor
+                                        .withOpacity(0.60),
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w500))
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              : ListView(children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      "Calendrier électoral de la CENI",
+                      style: GoogleFonts.roboto(
+                          textStyle: const TextStyle(
+                              color: AppColorTheme.textColor,
+                              fontSize: 26,
+                              fontWeight: FontWeight.bold)),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 24,
+                  ),
+                  Container(
+                    height: 250,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(4),
+                      boxShadow: [
+                        BoxShadow(
+                          color: AppColorTheme.darkColor.withOpacity(0.04),
+                          blurRadius: 20,
+                          offset: const Offset(0, 2),
+                        ),
+                      ],
+                      color: AppColorTheme.whiteColor,
+                    ),
+                    child: Image.asset(
+                      'assets/images/calendar.png',
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 24,
+                  ),
+                  ListView.builder(
+                      itemCount: controller.calandarsList.length,
+                      // scrollDirection: Axis.vertical,
+                      primary: false,
+                      shrinkWrap: true,
+                      itemBuilder: (context, index) {
+                        return CalandarCardWidget(
+                            calandarsData: controller.calandarsList[index]);
+                      })
+                ])
+          : ListView(
+              children: const [
+                NewsCardShimmerWidget(),
+              ],
+            ),
     );
   }
 }
--- lib/app/widgets/electoral_card/electoral_item_card_view.dart
+++ lib/app/widgets/electoral_card/electoral_item_card_view.dart
@@ -6,14 +6,21 @@ import 'package:mediaapp/app/core/themes/color_theme.dart';
 
 import '../../data/models/electoral_model.dart';
 
-class ElectoralItemCardView extends GetView {
+class ElectoralItemCardView extends StatefulWidget {
   const ElectoralItemCardView({Key? key, required this.electoralData})
       : super(key: key);
   final ElectoralData electoralData;
+
+  @override
+  State<ElectoralItemCardView> createState() => _ElectoralItemCardViewState();
+}
+
+class _ElectoralItemCardViewState extends State<ElectoralItemCardView> {
+  bool isExpanded = false;
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: double.infinity,
       clipBehavior: Clip.antiAlias,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(4),
@@ -27,33 +34,44 @@ class ElectoralItemCardView extends GetView {
         color: AppColorTheme.whiteColor,
       ),
       child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            child: Text(
-              electoralData.titre,
-              style: GoogleFonts.roboto(
-                  textStyle: const TextStyle(
-                      color: AppColorTheme.textColor,
-                      fontSize: 20,
-                      fontWeight: FontWeight.w500)),
-            ),
-          ),
-          const SizedBox(height: 8),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            child: Text(
-              electoralData.contenu,
-              textAlign: TextAlign.start,
-              style: GoogleFonts.roboto(
-                  textStyle: TextStyle(
-                      color: AppColorTheme.textColor.withOpacity(0.6),
-                      fontSize: 16,
-                      fontWeight: FontWeight.w400)),
-            ),
-          ),
-        ]),
+        padding: const EdgeInsets.all(8.0),
+        child: ExpansionPanelList(
+            elevation: 0,
+            expandedHeaderPadding: const EdgeInsets.all(0),
+            expansionCallback: (int index, bool isExpanded) {
+              setState(() {
+                this.isExpanded = !isExpanded;
+              });
+            },
+            children: [
+              ExpansionPanel(
+                isExpanded: isExpanded,
+                canTapOnHeader: true,
+                backgroundColor: Colors.transparent,
+                headerBuilder: (context, isExpanded) {
+                  return Text(
+                    widget.electoralData.titre,
+                    style: GoogleFonts.roboto(
+                        textStyle: const TextStyle(
+                            color: AppColorTheme.textColor,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500)),
+                  );
+                },
+                body: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    widget.electoralData.contenu * 30,
+                    textAlign: TextAlign.start,
+                    style: GoogleFonts.roboto(
+                        textStyle: TextStyle(
+                            color: AppColorTheme.textColor.withOpacity(0.6),
+                            fontSize: 16,
+                            fontWeight: FontWeight.w400)),
+                  ),
+                ),
+              )
+            ]),
       ),
     );
   }
