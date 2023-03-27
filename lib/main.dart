b01e1bc723b142d0c2e2059ac45c7f9b6ea00d7c
Christian Mwamba <chrismwamba9@gmail.com>
2023-03-27 16:44:46 +0200

    fire config

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,21 @@
+import 'package:flutter/material.dart';
 
+import 'package:get/get.dart';
+
+import 'app/core/themes/app_theme.dart';
+import 'app/routes/app_pages.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(
+    GetMaterialApp(
+      title: "MediaApp",
+      debugShowCheckedModeBanner: false,
+      theme: appThemeData,
+      initialRoute: '/welcome',
+      getPages: AppPages.routes,
+      defaultTransition: Transition.fade,
+    ),
+  );
+}
